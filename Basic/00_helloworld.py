1b13e3fad4c3c0e2aed96f5c7aba1f5c0ac5a5f5
DanyRJ <rjdany99@gmail.com>
2023-07-02 12:03:34 +0200

    :)

---
+++ Basic/00_helloworld.py
@@ -0,0 +1,19 @@
+# Esto es un comentario:
+# Buenas este es mi comienzo en Python
+print("Hola Python")
+print('Hola Python')
+
+"""
+Este  es un
+comentario
+en varias líneas
+"""
+
+# Consultar el tipo de dato
+print(type("Hola Python")) # Este tipo es 'str'
+print(type(5)) # Tipo 'int'
+print(type(1.5)) # Tipo 'float'
+print(type(1 + 2j)) # Tipo 'complex'
+print(type(True)) #Tipo 'bool
+
+print(type(print("Mi cadena de texto"))) #Tipo 'NoneType'
