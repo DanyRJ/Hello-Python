81626808611b406c701e52d34bd75e6c8f964aca
DanyRJ <rjdany99@gmail.com>
2023-07-07 17:38:12 +0200

    .

---
+++ Basic/02_operators.py
@@ -0,0 +1,38 @@
+### Operadores Aritméticos ###
+
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+print(10 % 2)
+print(10 // 3)
+print(2 ** 3)
+print(2**3+3-7/1//2)
+
+print("Hola" + "Phyton" + "¿Qué tal?")
+print("Hola"+str(5))
+print("Hola" * (2 ** 3))
+
+
+my_float = 2.5 * 2
+print("Hola " * int(my_float))
+
+### Operadores Comparativos ###
+
+print(3 > 4)
+print(3 < 4)
+print(3 >= 4)
+print(3 <= 4)
+print(3 == 4)
+print(3 != 4)
+
+print("Hola" > "Phyton")
+print("Hola" < "Phyton")
+print("aaaa" >= "abaa") # Ordenación alfabética por ASCII
+print(len("aaaa") >= len("abaa")) # Cuenta caracteres
+print("Hola" <= "Phyton")
+print("Hola" == "Phyton")
+print("Hola" != "Phyton")
+
+### Operadores Lógicos ###
+
