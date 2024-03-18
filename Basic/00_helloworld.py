fc846e38dc9b2e43548643ec8551b42b86fd7309
DanyRJ <rjdany99@gmail.com>
2024-03-18 13:27:00 +0100

    .

--- Basic/00_helloworld.py
+++ Basic/00_helloworld.py
@@ -1,5 +1,6 @@
 # Esto es un comentario:
 # Buenas este es mi comienzo en Python
+# Esto es un comentario de prueba, para saber si se actualiza con github
 print("Hola Python")
 print('Hola Python')
 
